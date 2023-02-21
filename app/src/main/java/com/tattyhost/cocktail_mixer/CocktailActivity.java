eddc4a5dcb828b59f39ba92d0c88ddb9d4956a3f
blombler008 <blombler008@gmail.com>
2023-02-21 13:37:21 +0100

    Made Base Layout

--- app/src/main/java/com/tattyhost/cocktail_mixer/CocktailActivity.java
+++ app/src/main/java/com/tattyhost/cocktail_mixer/CocktailActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class CocktailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+
+        setContentView(R.layout.activity_main);
     }
 }
