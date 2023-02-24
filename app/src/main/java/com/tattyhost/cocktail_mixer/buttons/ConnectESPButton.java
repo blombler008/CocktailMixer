88cdbd7cb9e97b3feb1e8141e3a185e489658d91
blombler008 <blombler008@gmail.com>
2023-02-24 21:53:18 +0100

    Added ESP tablet Communication

--- app/src/main/java/com/tattyhost/cocktail_mixer/buttons/ConnectESPButton.java
+++ app/src/main/java/com/tattyhost/cocktail_mixer/buttons/ConnectESPButton.java
@@ -1,6 +1,9 @@
 package com.tattyhost.cocktail_mixer.buttons;
 
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,19 +15,23 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class ConnectESPButton extends ButtonAction {
 
     private final Button button;
     private final TextView output;
-
+    private StringBuilder currentText = new StringBuilder();
     public ConnectESPButton(Button button, TextView output) {
         this.button = button;
         this.output = output;
+        currentText.append(output.getText());
     }
 
     @Override
@@ -38,15 +45,22 @@ public class ConnectESPButton extends ButtonAction {
         new NetworkTask(this).execute();
 
     }
+    private Handler mHandler = new Handler(Looper.getMainLooper());
 
     public void println(String s) {
-        String newBuild = output.getText() + s + "\n";
-        output.setText(newBuild);
+        currentText.append(s);
+        currentText.append('\n');
+        mHandler.post(() -> {
+            Log.i("OUTPUT ESP", "println: " + s);
+            output.setText(currentText.toString());
+        } );
     }
 }
 
 class NetworkTask extends AsyncTask<Void, Void, Void> {
     ConnectESPButton espOutPut;
+
+
 
     public NetworkTask(ConnectESPButton println) {
         this.espOutPut = println;
@@ -66,7 +80,7 @@ class NetworkTask extends AsyncTask<Void, Void, Void> {
             InetAddress broadcastAddress = InetAddress.getByName("255.255.255.255");
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, portUDP);
 
-            espOutPut.println("Sending UDP Packet: " + sendData);
+            espOutPut.println("Sending UDP Packet: " + new String(sendData));
             socket.send(sendPacket);
 
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -75,24 +89,46 @@ class NetworkTask extends AsyncTask<Void, Void, Void> {
             espOutPut.println("ESP32S2 IP-Adresse: " + ipAddress);
 
             Socket tcpSocket = new Socket(ipAddress, portTCP);
+            tcpSocket.setSoTimeout(24*1000);
             BufferedReader in = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(tcpSocket.getOutputStream())), true);
 
             // Hier können Sie Daten an den ESP32S2 senden und empfangen
             espOutPut.println("Sending TCP to ESP32S2: Hello ESP32S2!");
             out.println("Hello ESP32S2!");
-            String response = in.readLine();
-            espOutPut.println("Received from ESP32S2: " + response);
+            while (tcpSocket.isConnected()){
+                if(in.ready()) {
+                    String msg = in.readLine();
+                    int len =  msg.indexOf('\n');
+                    if(len == -1)
+                        len = msg.length();
 
+                    msg = msg.substring(0, len);
+                    espOutPut.println("Received from ESP32S2: " + msg);
+
+                    if(msg.equals("Hello Tablet!")) {
+                        close(tcpSocket, in, out);
+                        break;
+                    }
+                }
+            }
+            if(tcpSocket.isConnected())
+                close(tcpSocket, in, out);
             // Verbindung beenden
-            in.close();
-            out.close();
-            tcpSocket.close();
+            espOutPut.println("Verbindungen getrennt");
         } catch (Exception e) {
             espOutPut.println("EXCEPTION: " + e);
+            espOutPut.println("EXCEPTION: " + Arrays.toString(e.getStackTrace()));
+
 
         }
 
         return null;
+    }
+
+    private void close(Socket tcpSocket, Reader in, Writer out) throws Exception {
+        in.close();
+        out.close();
+        tcpSocket.close();
     }
 }
